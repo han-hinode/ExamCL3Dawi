06a3c54d6a9ba24a65f9bd5075a78a37a5ec3f22
VRAKILLCH <vrakillch@gmail.com>
2023-06-22 19:19:57 -0500

    improving the project

--- src/main/java/com/cibertec/exam/controllers/ProductoController.java
+++ src/main/java/com/cibertec/exam/controllers/ProductoController.java
@@ -1,5 +1,7 @@
 package com.cibertec.exam.controllers;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -10,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +20,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cibertec.exam.models.Producto;
 import com.cibertec.exam.services.ProductoServiceImpl;
+
+import net.sf.jasperreports.engine.JRException;
 
 @Controller
 public class ProductoController {
@@ -63,19 +68,19 @@ public class ProductoController {
 		if (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
 
 			try {
-				Producto objTweet = new Producto();
+				Producto objProducto = new Producto();
 
-				objTweet.setIdProducto(0L);
-				objTweet.setNomProducto(idNombre);
+				objProducto.setIdProducto(0L);
+				objProducto.setNomProducto(idNombre);
 				Date defaultValue = new Date();
-				objTweet.setFecProducto(defaultValue);
-				objTweet.setDesProducto(idDescripcion);
+				objProducto.setFecProducto(defaultValue);
+				objProducto.setDesProducto(idDescripcion);
 
-				productoServi.newProduct(objTweet);
+				productoServi.newProduct(objProducto);
 
 			} catch (Exception e) {
 				
-				redirect.addAttribute("mensaje", "Ocurrió un error al intentar guardar tweet!");
+				redirect.addAttribute("mensaje", "Ocurrió un error al intentar guardar el producto!");
 				e.printStackTrace();
 			}
 		}
@@ -83,5 +88,42 @@ public class ProductoController {
 			return "access_denied";
 		}
 		return "redirect:/home";
+	}
+	
+	@RequestMapping("/reporte/{format}")
+	public String generarReporte(@PathVariable String format, RedirectAttributes redirect) throws JRException, IOException {
+		try {
+			productoServi.exportarProducto(format);
+			System.out.println("Producto Generado");
+			if(format.equals("pdf")) {
+				abrirArchivoPdf();
+			}
+			if(format.equals("html")) {
+				abrirArchivoHtml();
+			}
+		} catch(Exception e) {
+			System.out.println("Error al generar producto");
+		}
+		return "redirect:/home";
+	}
+	
+	private void abrirArchivoHtml() {
+		try{
+			File path = new File("");
+			String directoryName = path.getAbsoluteFile().toString();
+			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler  "+ directoryName+"/reportes"+"/Producto.html");
+			}catch(IOException e1){
+			System.out.print(e1.toString());
+			}	
+	}
+
+	private void abrirArchivoPdf() {
+		try {
+			File path = new File("");
+			String directoryName = path.getAbsoluteFile().toString();
+		    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + directoryName+"/reportes"+"/Producto.pdf");
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/cibertec/exam/services/ProductoServiceImpl.java
+++ src/main/java/com/cibertec/exam/services/ProductoServiceImpl.java
@@ -1,18 +1,31 @@
 package com.cibertec.exam.services;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 import com.cibertec.exam.models.Producto;
 import com.cibertec.exam.repositories.ProductoRepository;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
 
 @Service
 public class ProductoServiceImpl implements ProductoService{
 	
 	@Autowired
-	private ProductoRepository productoRepo;
+	private ProductoRepository productoRepo;;
 
 	@Override
 	public List<Producto> getAllProducts() {
@@ -24,4 +37,28 @@ public class ProductoServiceImpl implements ProductoService{
 		productoRepo.save(producto);
 	}
 
+	public void exportarProducto(String format) throws JRException, IOException {
+		// RECUPERAMOS LA LISTA
+		List<Producto> listaProductos = productoRepo.findAll();
+		// OBTENIENDO RUTA ACTUAL DEL PROYECTO
+		File path = new File("");
+		String directoryName = path.getAbsolutePath().toString();
+		// LLENANDO LA TABLA DE JASPER
+		File file = ResourceUtils.getFile("classpath:jasper/ReporteProducto.jasper");
+		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(file);
+		JRBeanCollectionDataSource datasource = new JRBeanCollectionDataSource(listaProductos);
+		// LLENANDO ALGUN PARAMETRO QUE SE HAYA DEFINIDO EN JASPER
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("examen", "Examen CL3 Dawi");
+		//parameters.put("logo", directoryName+"\\src\\main\\resources\\static\\img\\package.png");
+		parameters.put("github", "https://github.com/vrakillch/ExamCL3Dawi.git");
+		// VISUALIZANDO LA TABLA DE JASPER
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, datasource);
+		if(format.equalsIgnoreCase("pdf")) {
+			JasperExportManager.exportReportToPdfFile(jasperPrint, directoryName+"\\reportes"+"\\Producto.pdf");
+		}
+		if(format.equalsIgnoreCase("html")) {
+			JasperExportManager.exportReportToHtmlFile(jasperPrint, directoryName+"\\reportes"+"\\Producto.html");
+		}
+	}
 }
